15859037b26ea5712d9eac6dfb11b13e65443f7e
parz1 <parz1zhou@outlook.com>
2022-12-11 02:18:39 +0800

    init nuxt blog

---
+++ app.config.ts
@@ -0,0 +1,3 @@
+export default defineAppConfig({
+  title: 'parz1\s land',
+})
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    // todo
+    return {}
+  },
+})
+</script>
+
+<template>
+  <div>init</div>
+</template>
+
+<style scoped></style>
+
