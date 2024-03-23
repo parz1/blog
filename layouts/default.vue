c7a5d4ee32c02350e949d2020d8fca64addacd13
parz1 <parz1zhou@outlook.com>
2024-03-23 19:35:16 +0800

    Add @vercel/speed-insights package and component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+</script>
 
 <template>
   <div>
     <Header />
     <slot />
   </div>
+  <SpeedInsights />
 </template>
