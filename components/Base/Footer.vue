ebf0a8d2dc7195b97c1a440087038640dc12d4ff
parz1 <parz1zhou@outlook.com>
2024-04-03 09:25:41 +0800

    Add new components and update existing components

---
+++ components/Base/Footer.vue
@@ -0,0 +1,22 @@
+<script lang='ts' setup>
+const thisYear = new Date().getFullYear()
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <div class="text-left px-4">
+      <div class="mt-4 text-4">
+        About Me
+      </div>
+      <div class="mt-4 text-blueGray">
+        全栈开发者，热爱技术，喜欢做梦。
+      </div>
+      <!-- copyright -->
+      <div class="mt-4 text-blueGray">
+        © {{ thisYear }} parz1
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang='scss' scoped></style>
